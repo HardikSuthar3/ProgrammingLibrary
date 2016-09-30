4bb5f0305682a6bfd45b24760d29787b73063493
HardikSuthar3 <hsuthar326@gmail.com>
2016-09-30 10:01:03 +0530

    Change

--- main.cpp
+++ main.cpp
@@ -1,28 +1,33 @@
 /*ifstream in("/HSData/Data/IITM/workspace/CLion/ProblemsImplementation/input.txt", ios::in);
     auto buff = std::cin.rdbuf();
     std::cin.rdbuf(in.rdbuf());*/
-#include "DP/Optimal Strategy for a Game.h"
+#include<bits/stdc++.h>
 
 using namespace std;
-int sol[100][100];
+
+
 
 int main() {
+
     freopen("/HSData/Data/IITM/workspace/CLion/ProgrammingLibrary/input.txt", "r", stdin);
-    freopen("/HSData/Data/IITM/workspace/CLion/ProgrammingLibrary/output.txt", "w", stdout);
+//    freopen("/HSData/Data/IITM/workspace/CLion/ProgrammingLibrary/output.txt", "w", stdout);{
 
-    clock_t t = clock();
-    int n;
-    cin >> n;
-    int *a = new int[n];
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
-        for (int j = 0; j < n; ++j) {
-            sol[i][j] = -1;
-        }
-    }
 
-    OptimalStrategy(a, n);
 
-    cerr << (float) (clock() - t) / CLOCKS_PER_SEC;
+
+    // Input Begin
+
+
+    // Input End
+
+
+    // Sol Begin
+
+    // Sol End
+
+
+
+
     return 0;
+
 }
