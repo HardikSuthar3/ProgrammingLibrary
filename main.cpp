04102e4a7924994a598773a1a881869018a7dd0d
HardikSuthar3 <hsuthar326@gmail.com>
2016-10-02 23:59:17 +0530

    Change

--- main.cpp
+++ main.cpp
@@ -1,16 +1,28 @@
 /*ifstream in("/HSData/Data/IITM/workspace/CLion/ProblemsImplementation/input.txt", ios::in);
     auto buff = std::cin.rdbuf();
     std::cin.rdbuf(in.rdbuf());*/
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main() {
-
-//    freopen("/HSData/Data/IITM/workspace/CLion/ProgrammingLibrary/input.txt", "r", stdin);
+    freopen("/HSData/Data/IITM/workspace/CLion/ProgrammingLibrary/input.txt", "r", stdin);
     freopen("/HSData/Data/IITM/workspace/CLion/ProgrammingLibrary/output.txt", "w", stdout);
-    int x;
-    for (int i = 0; i < 20; i++) {
-        cout << i++ << " ";
+
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        // Input Begin
+
+        // Input End
+
+
+        // Sol Begin
+
+        // Sol End
     }
+
+    return 0;
 }
