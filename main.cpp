0f7c11f3262d8e94b0494cb9a12250f795e4134b
HardikSuthar3 <hsuthar326@gmail.com>
2016-10-08 10:46:56 +0530

    Change

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include<bits/stdc++.h>
+#include "Compnies/Adobe/A.cpp"
+#include "Compnies/Adobe/B.cpp"
+#include "Compnies/Adobe/C.cpp"
 
 using namespace std;
 
-
 int main() {
-    freopen("/HSData/Data/IITM/workspace/CLion/ProgrammingLibrary/input.txt", "r", stdin);
-    cout << (4 >> 1 + 1);
+//    freopen("/HSData/Data/IITM/workspace/CLion/ProgrammingLibrary/input.txt", "r", stdin);
+
     return 0;
 }
