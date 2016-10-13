5318f9c0d338728d73230dba38490cfb888895ed
hardik_suthar <hsuthar326@gmail.com>
2016-10-13 19:47:04 +0530

    Change

--- main.cpp
+++ main.cpp
@@ -1,38 +1,19 @@
 #include<bits/stdc++.h>
+#include <numeric>
 
 using namespace std;
 
-int coinChange(vector<int> &coins, int amount) {
-    vector<int> dp(amount + 1, INT_MAX);
-    dp[0] = 0;
-    for (int i = 1; i <= amount; i++) {
-        for (int j = 0; j < coins.size(); j++) {
-            if (coins[j] <= i) {
-                dp[i] = min(dp[i - coins[j]] + 1, dp[i]);
-            }
-        }
-        cout << dp[i] << " ";
-    }
-
-
-    return dp[amount] > amount|| dp[amount] <0? -1 : dp[amount];
+int gcd(int a, int b) {
+    if (a == 0)
+        return b;
+    return gcd(b % a, a);
 }
 
 int main() {
 //    freopen("/HSData/Data/IITM/workspace/CLion/ProgrammingLibrary/input.txt", "r", stdin);
-
-
     freopen("/home/hardik/ClionProjects/ProgrammingLibrary/input.txt", "r", stdin);
+//    freopen("/home/hardik/ClionProjects/ProgrammingLibrary/output.txt", "w", stdout);
 
-    int n, sum;
-    cin >> n;
-    vector<int> a(n);
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
-    }
-    cin >> sum;
-
-    cout << coinChange(a, sum);
 
 
     return 0;
