d1571b8e904732b038230e5a738efbe840172c2e
HardikSuthar3 <hsuthar326@gmail.com>
2016-10-25 03:14:14 +0530

    Change

--- main.cpp
+++ main.cpp
@@ -1,46 +1,70 @@
 #include<bits/stdc++.h>
 
 using namespace std;
-#define MOD 1000000007
 
-inline void add(int &a, int b) {
-    a += b;
-    if (a >= MOD) {
-        a -= MOD;
-    }
-}
-
-inline int mul(int a, int b) {
-    return (long long) (a * b) % MOD;
-}
-
-inline int power(int a, int b) {
-    int res = 1;
-    while (b > 0) {
-        if (b & 1) {
-            res = mul(res, a);
+bool isBalanced(string str) {
+    stack<char> st;
+    for (auto &item:str) {
+        if (item == '(') {
+            st.push(item);
+        } else {
+            if (st.empty() == true)
+                return false;
+            else {
+                st.pop();
+            }
         }
-        b = b >> 1;
-        a = mul(a, a);
     }
-    return res;
+    return st.empty();
 }
 
-inline int inv(int x) {
-    return power(x, MOD - 2);
+set<string> ss;
+
+void change(string str, int start) {
+    if (str.size() == 0)
+        return;
+    if (start >= str.size()) {
+        if (isBalanced(str)) {
+            ss.insert(str);
+        }
+    }
+    int c = 0;
+    for (int i = start; i < str.size(); i++) {
+        if (str[i] == '*') {
+            c++;
+            str[i] = '(';
+            change(str, i + 1);
+
+            str[i] = ')';
+            change(str, i + 1);
+
+            string a = str.substr(0, i) + str.substr(i + 1);
+            change(a, i);
+        }
+    }
+    if (c == 0 && isBalanced(str)) {
+        ss.insert(str);
+    }
 }
 
 int main() {
-    freopen("/home/hardik/ClionProjects/ProgrammingLibrary/input.txt", "r", stdin);
-    int n;
-    cin >> n;
-    vector<int> a(n);
-    int sum = 0;
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
-        sum = (sum + a[i]) % MOD;
+    freopen("/HSData/Data/IITM/workspace/CLion/ProgrammingLibrary/output.txt", "w", stdout);
+    string s = "(*(*)*)";
+    s = "*(*(**)*";
+    s = "";
+    change(s, 0);
+    for (auto &item:ss) {
+        cout << item << endl;
     }
-    int total = (sum * a.size()) % MOD;
-    int left = a[0];
+
+    priority_queue<int, deque<int> > q;
+    for (int i = 1; i <= 10; i++) {
+        q.push(rand() % 11);
+    }
+    for (int i = 0; i < 10; i++) {
+        cout << q.top() << " ";
+        q.pop();
+    }
     return 0;
 }
+
