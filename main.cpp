387966abc9ba1fb910b821772e4b7b084bc0390e
HardikSuthar3 <hsuthar326@gmail.com>
2016-10-27 19:59:19 +0530

    Change

--- main.cpp
+++ main.cpp
@@ -2,69 +2,18 @@
 
 using namespace std;
 
-bool isBalanced(string str) {
-    stack<char> st;
-    for (auto &item:str) {
-        if (item == '(') {
-            st.push(item);
-        } else {
-            if (st.empty() == true)
-                return false;
-            else {
-                st.pop();
-            }
-        }
-    }
-    return st.empty();
-}
+vector<int> Graph[100];
+vector<int, pair<int, int>> edges;
+vector<bool> used;
 
-set<string> ss;
+void MakeGraph(int n, int m) {
+    fill(used.begin(), used.begin() + n + 1, false);
 
-void change(string str, int start) {
-    if (str.size() == 0)
-        return;
-    if (start >= str.size()) {
-        if (isBalanced(str)) {
-            ss.insert(str);
-        }
-    }
-    int c = 0;
-    for (int i = start; i < str.size(); i++) {
-        if (str[i] == '*') {
-            c++;
-            str[i] = '(';
-            change(str, i + 1);
-
-            str[i] = ')';
-            change(str, i + 1);
-
-            string a = str.substr(0, i) + str.substr(i + 1);
-            change(a, i);
-        }
-    }
-    if (c == 0 && isBalanced(str)) {
-        ss.insert(str);
-    }
 }
 
 int main() {
     freopen("/HSData/Data/IITM/workspace/CLion/ProgrammingLibrary/output.txt", "w", stdout);
-    string s = "(*(*)*)";
-    s = "*(*(**)*";
-    s = "";
-    change(s, 0);
-    for (auto &item:ss) {
-        cout << item << endl;
-    }
 
-    priority_queue<int, deque<int> > q;
-    for (int i = 1; i <= 10; i++) {
-        q.push(rand() % 11);
-    }
-    for (int i = 0; i < 10; i++) {
-        cout << q.top() << " ";
-        q.pop();
-    }
     return 0;
 }
 
