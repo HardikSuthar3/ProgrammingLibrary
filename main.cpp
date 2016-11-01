4d72d4648346a3129ee4f78294abe9f86ecb1ee7
HardikSuthar3 <hsuthar326@gmail.com>
2016-11-01 11:07:45 +0530

    Change

--- main.cpp
+++ main.cpp
@@ -2,18 +2,6 @@
 
 using namespace std;
 
-vector<int> Graph[100];
-vector<int, pair<int, int>> edges;
-vector<bool> used;
-
-void MakeGraph(int n, int m) {
-    fill(used.begin(), used.begin() + n + 1, false);
-
-}
-
 int main() {
-    freopen("/HSData/Data/IITM/workspace/CLion/ProgrammingLibrary/output.txt", "w", stdout);
-
     return 0;
 }
-
