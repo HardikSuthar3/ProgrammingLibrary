1741b508a428582f3b4a653ccafe90039c320431
HardikSuthar3 <hsuthar326@gmail.com>
2016-12-07 05:25:15 +0530

    Change

--- main.cpp
+++ main.cpp
@@ -2,35 +2,11 @@
 
 using namespace std;
 
-void fun(vector<string> &inp) {
-   int n = inp.size();
-   unordered_map<int, set<int>> data;
-   unordered_set<int> yvals;
-   for (int i = 0; i < n; i++) {
-      stringstream ss(inp[i]);
-      int x, y;
-      ss >> x >> y;
-      if (yvals.find(y) != yvals.end()) {
-         data[y].insert(x);
-      } else {
-         set<int> xset;
-         xset.insert(x);
-         yvals.insert(y);
-         data.insert(make_pair(y, xset));
-      }
-   }
-}
-
 int main() {
-   int n;
-   cin >> n;
-   vector<string> input;
-   cin.ignore();
-   for (int i = 0; i < n; i++) {
-      string str;
-      getline(cin, str);
-      input.push_back(str);
-   }
-   fun(input);
-   return 0;
+    vector<int> a(100);
+    for (int i = 0; i < 100; i++) {
+        cout << i << " ";
+    }
+
+    return 0;
 }
