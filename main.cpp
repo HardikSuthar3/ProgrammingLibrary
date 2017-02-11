49a901364fbc30b2184f7a092544355419de6b17
HardikSuthar3 <hsuthar326@gmail.com>
2017-02-11 11:42:02 +0530

    Change

--- main.cpp
+++ main.cpp
@@ -1,21 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    string str;
-    getline(cin, str);
-    reverse(str.begin(), str.end());
-    vector<int> res(str.length(), 0);
-    for(int i = 0; i < str.length(); i++) {
-        if((str[i] - '0') % 2 == 0) {
-            res[i] = res[i - 1] + 1;
-        } else {
-            res[i] = res[i - 1];
+bool isPrime(int x) {
+    if(x <= 2) {
+        return true;
+    }
+    for(int i = 2; i <= (int)sqrt(x); i++) {
+        if(x % i == 0) {
+            return false;
         }
     }
-    reverse(res.begin(), res.end());
-    for(auto &item: res) {
-        cout << item << " ";
+    return true;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> numbers(n + 1);
+    numbers[0] = 0;
+    for(int i = 1; i < n; i++) {
+        int val;
+        cin >> val;
+        numbers[i] = numbers[i - 1] + val;
+    }
+
+    for(int window = n - 1; window >= 0; window--) {
+        if(isPrime(window)) {
+            break;
+        }
     }
     return 0;
 }
