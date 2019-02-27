656dccaf57ab4480ba9d5c5bb66be58d9ef11c5b
hs_me <hsuthar326@gmail.com>
2019-02-27 16:52:03 +0530

    Change from Desk

--- main.cpp
+++ main.cpp
@@ -5,22 +5,12 @@ using namespace std;
 int main()
 {
     freopen("D:\\OneDrive - Microsoft\\Development\\C++\\ProgrammingLibrary\\input.txt", "r", stdin);
-    freopen("D:\\OneDrive - Microsoft\\Development\\C++\\ProgrammingLibrary\\output.txt", "w", stdout);
+    // freopen("D:\\OneDrive - Microsoft\\Development\\C++\\ProgrammingLibrary\\output.txt", "w", stdout);
 
     //Reading input
     int n, q;
     cin >> n >> q;
 
-    vector<pair<pair<int, int>, int>> num;
-    for (size_t i = 0; i < q; i++)
-    {
-        int x, y, z;
-        cin >> x >> y >> z;
-        auto tmp = make_pair(make_pair(x, y), z);
-        num.push_back(tmp);
-    }
-
-    // Program logic
-
+    cout << n << " " << q;
     return 0;
 }
