fe5251fe2670e58f7fd422c9bc4413c6c5bceab5
Hardik Suthar <hasuthar@microsoft.com>
2019-03-05 17:37:13 +0530

    Change from Lappy

--- main.cpp
+++ main.cpp
@@ -7,10 +7,26 @@ int main()
     freopen("D:\\OneDrive - Microsoft\\Development\\C++\\ProgrammingLibrary\\input.txt", "r", stdin);
     // freopen("D:\\OneDrive - Microsoft\\Development\\C++\\ProgrammingLibrary\\output.txt", "w", stdout);
 
-    //Reading input
-    int n, q;
-    cin >> n >> q;
+    vector<int> num;
 
-    cout << n << " " << q;
+    //Reading input
+    for (size_t i = 0; i < 10; i++)
+    {
+        num.push_back(rand() % 100);
+    }
+
+    cout << "Before Sort" << endl;
+    for (auto &&i : num)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+    cout << "After Sort" << endl;
+    sort(num.begin(), num.end(), std::less<int>());
+    for (auto &&i : num)
+    {
+        cout << i << " ";
+    }
+
     return 0;
 }
