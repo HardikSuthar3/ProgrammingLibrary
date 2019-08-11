a8a1672acfc8e736288afe57a857df609e1119a7
Hardik Suthar <hsuthar326@gmail.com>
2019-08-11 11:57:00 +0530

    From Mac Repo

--- Library/Graph_Structure.h
+++ Library/Graph_Structure.h
@@ -2,7 +2,6 @@
 
 using namespace std;
 
-
 // Graph Structure
 int V, E, root;
 vector<int> G[1001];
@@ -12,16 +11,18 @@ int *par;
 bool *used;
 // End Structure
 
-
 // Common Function
 
-void UFinit(int n) {
-    for (int i = 0; i <= n; i++) {
+void UFinit(int n)
+{
+    for (int i = 0; i <= n; i++)
+    {
         par[i] = i;
     }
 }
 
-void MakeGraph(int n, int m) {
+void MakeGraph(int n, int m)
+{
     V = n;
     E = m;
 
@@ -30,56 +31,65 @@ void MakeGraph(int n, int m) {
     fill(par, par + n + 1, -1);
     fill(used, used + n + 1, false);
 
-    for (int i = 0; i < m; i++) {
+    for (int i = 0; i < m; i++)
+    {
         int u, v, w;
         cin >> u >> v >> w;
         G[u].push_back(i);
 
-//         comment below to make directed graph
-//        G[v].push_back(i);
+        //         comment below to make directed graph
+        //        G[v].push_back(i);
 
         edges.push_back(make_pair(u, v));
         W.push_back(make_pair(i, w));
     }
 }
 
-void PrintGraph() {
+void PrintGraph()
+{
     cout << "Nodes" << endl;
-    for (int i = 0; i < V; i++) {
+    for (int i = 0; i < V; i++)
+    {
         cout << i << " : ";
-        for (int j = 0; j < G[i].size(); j++) {
+        for (int j = 0; j < G[i].size(); j++)
+        {
             int eid = G[i][j];
             auto x = edges[eid];
-            int adj = x.first ^x.second ^i;
+            int adj = x.first ^ x.second ^ i;
             cout << adj << " ";
         }
         cout << endl;
     }
-    cout << endl << endl;
+    cout << endl
+         << endl;
     cout << "Edges" << endl;
-    for (int i = 0; i < edges.size(); i++) {
+    for (int i = 0; i < edges.size(); i++)
+    {
         cout << edges[i].first << " " << edges[i].second << ": " << W[i].second << endl;
     }
-
 }
 
-void ClearGraph() {
-    delete[]par;
+void ClearGraph()
+{
+    delete[] par;
     delete[] used;
-    for (int i = 0; i < V; i++) {
+    for (int i = 0; i < V; i++)
+    {
         G[i].clear();
-
     }
     edges.clear();
     W.clear();
     V = E = root = -1;
 }
 
-void TransposeGraph() {
-    for (int i = 0; i < V; i++) {
+void TransposeGraph()
+{
+    for (int i = 0; i < V; i++)
+    {
         G[i].clear();
     }
-    for (int i = 0; i < E; i++) {
+    for (int i = 0; i < E; i++)
+    {
         swap(edges[i].first, edges[i].second);
         G[edges[i].first].push_back(i);
 
@@ -88,13 +98,15 @@ void TransposeGraph() {
     }
 }
 
-int Find(int x) {
+int Find(int x)
+{
     if (par[x] == x)
         return x;
     return par[x] = Find(par[x]);
 }
 
-void Union(int x, int y) {
+void Union(int x, int y)
+{
     int xp, yp;
     xp = Find(x);
     yp = Find(y);
