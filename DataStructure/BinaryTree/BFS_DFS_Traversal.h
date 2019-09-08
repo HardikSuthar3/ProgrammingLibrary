eef4a2802dfa9c9de18d45b51f8554fea9bd20b5
Hardik Suthar <hsuthar326@gmail.com>
2019-09-08 22:04:24 +0530

    Change from Mac
    Indentation Change

--- DataStructure/BinaryTree/BFS_DFS_Traversal.h
+++ DataStructure/BinaryTree/BFS_DFS_Traversal.h
@@ -1,7 +1,7 @@
 #include "BinaryTreeStructure.h"
 
-
-vector<int> dfs(Node *r) {
+vector<int> dfs(Node *r)
+{
     static vector<int> dfsOrder;
     if (r == nullptr)
         return dfsOrder;
@@ -11,7 +11,8 @@ vector<int> dfs(Node *r) {
     return dfsOrder;
 }
 
-vector<int> bfs(Node *r) {
+vector<int> bfs(Node *r)
+{
     vector<int> bfsOrder;
     queue<Node *> q;
     if (r == nullptr)
@@ -20,14 +21,17 @@ vector<int> bfs(Node *r) {
     q.push(r);
 
     r->visited = true;
-    while (!q.empty()) {
+    while (!q.empty())
+    {
         Node *u = q.front();
         q.pop();
-        if (u->left != nullptr && !u->left->visited) {
+        if (u->left != nullptr && !u->left->visited)
+        {
             q.push(u->left);
             u->left->visited = true;
         }
-        if (u->right != nullptr && !u->right->visited) {
+        if (u->right != nullptr && !u->right->visited)
+        {
             q.push(u->right);
             u->right->visited = true;
         }
