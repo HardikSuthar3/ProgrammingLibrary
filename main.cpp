4c41e5bc97af60d848aab1ec057e25792a9e5dbe
Hardik Suthar <hasuthar@microsoft.com>
2020-03-02 10:34:27 +0530

    Change-Lappy
    Chagnge from Lappy

--- main.cpp
+++ main.cpp
@@ -3,5 +3,19 @@ using namespace std;
 
 int main()
 {
+    unordered_map<string, int> items;
+
+    items.insert(make_pair("A1", 1));
+    items.insert(make_pair("A2", 2));
+    items.insert(make_pair("A3", 3));
+    items.insert(make_pair("A4", 4));
+
+    for (auto &&i : items)
+    {
+        string a = i.first;
+        int b = i.second;
+        cout << a << " " << b << endl;
+    }
+
     return 0;
 }
