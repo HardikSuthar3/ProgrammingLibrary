a442c6c4a7074b4e095cd2c76f7492f4daae89a3
Hardik_Suthar <hsuthar326@gmail.com>
2020-12-22 00:18:33 +0530

    Change from MAC

--- main.cpp
+++ main.cpp
@@ -1,39 +1,22 @@
 #include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
 int main()
 {
 
-    //freopen("/Users/hardik/HData/Development/CPP/ProgrammingLibrary/input.txt", "r", stdin);
+    freopen("/Users/hardik/HData/Development/CPP/ProgrammingLibrary/input.txt", "r", stdin);
 
     //Input Variable Declaration
-    int n;
-    vector<string> names;
-    cin >> n;
-    string tmp2;
-    getline(std::cin, tmp2);
-    for (size_t i = 0; i < n; i++)
-    {
-        string tmp;
-        std::getline(std::cin, tmp);
-        names.push_back(tmp);
-    }
-    cout << endl;
-    // Logic
+    string str;
+    getline(std::cin, str);
 
-    for (size_t i = 0; i < n; i++)
-    {
-        int count = 0;
-        for (size_t j = 0; j < i; j++)
-        {
-            if (std::lexicographical_compare(names[j].begin(), names[j].end(), names[i].begin(), names[i].end()))
-            {
-                count++;
-            }
-        }
-        cout << count << endl;
-    }
+    // Logic
+    cout << "Main String : " << str.length() << endl
+         << endl;
+
+    cout << "Rotated String : " << str << endl;
 
     // Output Section
 }
