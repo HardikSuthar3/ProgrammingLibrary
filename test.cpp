0bceb647fa77dee73ddc149d2ceedbb444cbd911
Hardik_Suthar <hsuthar326@gmail.com>
2021-04-26 19:42:04 +0530

    Change from Macbook Pro
    Change from MAC

--- test.cpp
+++ test.cpp
@@ -2,154 +2,43 @@
 
 using namespace std;
 
-typedef pair<int, int> iPair;
-
-class Graph
-{
-public:
-    int V, E;
-    vector<pair<int, iPair>> edges;
-    Graph(int v, int e);
-    void addEdge(int u, int v, int w)
-    {
-        edges.push_back(make_pair(w, make_pair(u, v)));
-    }
-    ~Graph();
-
-    int kruskalMST();
-};
-
-Graph::Graph(int v, int e)
-{
-    this->V = v;
-    this->E = e;
-}
-
-Graph::~Graph()
-{
-}
-
-// To represent Disjoint Sets
-class DisjointSets
-{
-public:
-    int *parent, *rnk;
-    int n;
-
-    // Constructor.
-    DisjointSets(int n)
-    {
-        // Allocate memory
-        this->n = n;
-        parent = new int[n + 1];
-        rnk = new int[n + 1];
-
-        // Initially, all vertices are in
-        // different sets and have rank 0.
-        for (int i = 0; i <= n; i++)
-        {
-            rnk[i] = 0;
-
-            //every element is parent of itself
-            parent[i] = i;
-        }
-    }
-
-    // Find the parent of a node 'u'
-    // Path Compression
-    int find(int u)
-    {
-        /* Make the parent of the nodes in the path 
-           from u--> parent[u] point to parent[u] */
-        if (u != parent[u])
-            parent[u] = find(parent[u]);
-        return parent[u];
-    }
-
-    // Union by rank
-    void merge(int x, int y)
-    {
-        x = find(x), y = find(y);
-
-        /* Make tree with smaller height 
-           a subtree of the other tree  */
-        if (rnk[x] > rnk[y])
-            parent[y] = x;
-        else // If rnk[x] <= rnk[y]
-            parent[x] = y;
-
-        if (rnk[x] == rnk[y])
-            rnk[y]++;
-    }
-};
-
-int Graph::kruskalMST()
-{
-    int mst_wt = 0; // Initialize result
-
-    // Sort edges in increasing order on basis of cost
-    sort(edges.begin(), edges.end());
-
-    // Create disjoint sets
-    DisjointSets ds(V);
-
-    // Iterate through all sorted edges
-    vector<pair<int, iPair>>::iterator it;
-    for (it = edges.begin(); it != edges.end(); it++)
-    {
-        int u = it->second.first;
-        int v = it->second.second;
-
-        int set_u = ds.find(u);
-        int set_v = ds.find(v);
-
-        // Check if the selected edge is creating
-        // a cycle or not (Cycle is created if u
-        // and v belong to same set)
-        if (set_u != set_v)
-        {
-            // Current edge will be in the MST
-            // so print it
-            cout << u << " - " << v << endl;
-
-            // Update MST weight
-            mst_wt += it->first;
-
-            // Merge two sets
-            ds.merge(set_u, set_v);
-        }
-    }
-
-    return mst_wt;
-}
-
 int main()
 {
-    /* Let us create above shown weighted 
-       and unidrected graph */
-    int V = 9, E = 14;
-    Graph g(V, E);
+    string input_time;
+    getline(cin, input_time);
 
-    //  making above shown graph
-    g.addEdge(0, 1, 4);
-    g.addEdge(0, 7, 8);
-    g.addEdge(1, 2, 8);
-    g.addEdge(1, 7, 11);
-    g.addEdge(2, 3, 7);
-    g.addEdge(2, 8, 2);
-    g.addEdge(2, 5, 4);
-    g.addEdge(3, 4, 9);
-    g.addEdge(3, 5, 14);
-    g.addEdge(4, 5, 10);
-    g.addEdge(5, 6, 2);
-    g.addEdge(6, 7, 1);
-    g.addEdge(6, 8, 6);
-    g.addEdge(7, 8, 7);
+    int hour_time = stoi(input_time.substr(0, 2));
 
-    cout << "Edges of MST are \n";
-    int mst_wt = g.kruskalMST();
+    string time_phase = input_time.substr(input_time.length() - 2, 2);
+    string new_time;
 
-    cout << "\nWeight of MST is " << mst_wt;
-
-    return 0;
+    if (time_phase == "AM")
+    {
+        new_time = to_string(hour_time);
+        if (hour_time == 12)
+        {
+            //replace 12 with 00
+            new_time = "00";
+        }
+        else
+        {
+            new_time = "0" + new_time;
+        }
+        new_time.append(input_time.begin() + 2, input_time.end() - 2);
+    }
+    else
+    {
+        if (hour_time != 12)
+        {
+            hour_time += 12;
+        }
+        new_time = to_string(hour_time);
+        new_time.append(input_time.begin() + 2, input_time.end() - 2);
+    }
+    cout << new_time;
 }
+
+//12:05:45AM
+//01:00:46AM
+//07:05:45PM
+//11:55:00PM
