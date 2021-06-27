b38db24b1b95033eb503e10f690d3c6fc004e200
Hardik_Suthar <hsuthar326@gmail.com>
2021-06-27 21:41:26 +0530

    Changes from MAC

--- test.cpp
+++ test.cpp
@@ -1,74 +1,37 @@
 #include <bits/stdc++.h>
+
 using namespace std;
-
-#define MAX (int)1e5
-int a[MAX];
-
-// Index = Root Node
-// N = Total Node
-void Heapify(int index, int n)
-{
-    // Compare current node with parent
-    int parent = (index - 1) / 2;
-    if (a[index] > a[parent])
-        swap(a[index], a[parent]);
-
-    // Repeat for Childs
-    int left = 2 * index + 1;
-    int right = 2 * index + 2;
-    if (left < n)
-        Heapify(left, n);
-
-    if (right < n)
-        Heapify(right, n);
-}
-
-void HeapSort(int root, int n)
-{
-    // 1. Heapify Tree
-    int left = 2 * root + 1;
-    int right = 2 * root + 2;
-    if (left < n)
-        Heapify(left, n);
-
-    if (right < n)
-        Heapify(right, n);
-
-    for (size_t i = 0; i < n - 1; i++)
-    {
-        // 2. Print Root
-        cout << a[root] << " ";
-
-        // 3. Swap Last Element With Root
-        swap(a[root], a[n - i - 1]);
-
-        // 4. Heapify with reduced size (n-i)
-        int left = 2 * i + 1;
-        int right = 2 * i + 2;
-        if (left < n)
-            Heapify(left, n - i);
-
-        if (right < n)
-            Heapify(right, n - i);
-    }
-
-    // 5. Repeat from Step 2
-}
 
 int main()
 {
-    freopen("t.txt", "r", stdin);
+    freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 
-    // Read Inputs
-    int n;
-    cin >> n;
-    for (size_t i = 0; i < n; i++)
+    int N;
+    cin >> N;
+    vector<int> a(N);
+    // priority_queue<int, vector<int>, std::greater<int>> pq, t_pq;
+    priority_queue<int, vector<int>, std::less<int>> pq, t_pq;
+
+    for (size_t i = 0; i < N; i++)
     {
         cin >> a[i];
+        pq.push(a[i]);
     }
 
-    HeapSort(0, n);
+    for (size_t i = 0; i < N; i++)
+    {
+        t_pq.push(a[i]);
+
+        while (!pq.empty() && !t_pq.empty() && (pq.top() == t_pq.top()))
+        {
+            cout << t_pq.top() << " ";
+            pq.pop();
+            t_pq.pop();
+        }
+
+        cout << endl;
+    }
 
     return 0;
 }
