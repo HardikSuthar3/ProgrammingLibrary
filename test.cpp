430c78fd866455fc8804e9393a140546629e0def
Hardik Suthar <hsuthar326@gmail.com>
2023-01-18 00:17:02 +0530

    Change from MAC

--- test.cpp
+++ test.cpp
@@ -2,29 +2,56 @@
 
 using namespace std;
 
+vector<int> breakingRecords(vector<int> scores)
+{
+    int min = scores[0];
+    int max = scores[0];
+    int minCount = 0;
+    int maxCount = 0;
+
+    for (int i = 1; i < scores.size(); i++)
+    {
+        if (scores[i] < min)
+        {
+            min = scores[i];
+            minCount++;
+        }
+        else if (scores[i] > max)
+        {
+            max = scores[i];
+            maxCount++;
+        }
+    }
+
+    vector<int> result;
+    result.push_back(maxCount);
+    result.push_back(minCount);
+    return result;
+}
+
 int main()
 {
+    // Change stdin to read from a file
     freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    vector<int> num;
+    // Change stdout to write to a file
+    // freopen("output.txt", "w", stdout);
 
-    for (int i = 0; i < 10; i++)
+    // Read vector
+    int n;
+    cin >> n;
+    vector<int> scores(n);
+    for (int i = 0; i < n; i++)
     {
-        num.push_back(rand() % 27);
+        cin >> scores[i];
     }
 
-    for (auto &&i : num)
-    {
-        cout << i << " ";
-    }
-
-    sort(num.begin(), num.end());
-
+    auto result = breakingRecords(scores);
     cout << endl;
-    for (auto &&i : num)
-    {
-        cout << i << " ";
-    }
 
+    // Print vector
+    for (int i = 0; i < result.size(); i++)
+    {
+        cout << result[i] << " ";
+    }
     return 0;
 }
