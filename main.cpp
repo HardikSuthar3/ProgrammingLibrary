7aee9c766a02723431cbf0efc7a4d4544d694bce
Hardik Suthar <hsuthar326@gmail.com>
2023-02-28 22:52:41 +0530

    Changs from Mac

--- main.cpp
+++ main.cpp
@@ -1,75 +1,11 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
 
 using namespace std;
 
-long long dp[1000];
+// main function
 
-// Graph Structure
-int V, E, root;
-vector<int> G[100];
-vector<pair<int, int>> edges;
-vector<pair<int, float>> W;
-// End Graph Structure
-
-void MakeGraph(int n, int m)
+int main(int argc, char const *argv[])
 {
-    V = n;
-    E = m;
-
-    for (int i = 0; i < m; i++)
-    {
-        int a, b, c;
-        cin >> a >> b >> c;
-        G[a].push_back(i);
-        edges.push_back(make_pair(a, b));
-        W.push_back(make_pair(i, c));
-    }
-}
-
-void PrintGraph()
-{
-    int n = V;
-    int m = edges.size();
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < G[i].size(); j++)
-        {
-            auto tmp = G[i][j];
-            auto u = edges[tmp].first;
-            auto v = edges[tmp].second;
-            auto w = W[tmp].second;
-        }
-    }
-}
-
-int main()
-{
-    // auto in2 = freopen("/Users/hardik/HData/Development/CPP/ProgrammingLibrary/input.txt", "r", stdin);
-    // auto o = freopen("/Users/hardik/HData/Development/CPP/ProgrammingLibrary/output.txt", "r", stdout);
-
-    vector<int> a;
-    for (int i = 0; i < 100; i++)
-    {
-        a.push_back(rand() % 29);
-    }
-
-    cout << "Before Sort" << endl;
-
-    for (auto &item : a)
-    {
-        cout << item << " ";
-    }
-
-    cout << endl
-         << endl
-         << "After Sort" << endl
-         << endl;
-    sort(a.begin(), a.end(), std::greater<int>());
-
-    for (auto &item : a)
-    {
-        cout << item << " ";
-    }
+    // Read N numbers in a vector
     return 0;
 }
