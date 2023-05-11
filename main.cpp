4d64021b36484edc08bb1a73f000865bb6ae9a2b
hs_me <hsuthar326@gmail.com>
2023-05-11 10:34:59 +0530

    Change from Mac

--- main.cpp
+++ main.cpp
@@ -1,25 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Heapify a subtree rooted with node i
-// which is an index in arr[]. n is size of heap
-void heapify(int arr[], int n)
-{
-    int i = n - 1;
-    int parent = (i - 1) / 2;
-    while (parent >= 0)
-    {
-        if (arr[parent] < arr[i])
-        {
-            swap(arr[parent], arr[i]);
-            i = parent;
-            parent = (i - 1) / 2;
-        }
-        else
-            break;
-    }
-}
-
 int main()
 {
+    int sum = add(1, 2, 3);
+    return sum;
+}
+
+void heapify(vector<int> test)
+{
+    // Heapify the vector test.
+    make_heap(test.begin(), test.end());
+}
+ 
+/* make heap from a vector of Integer */
+
+
+int add(int x, int y, int z)
+{
+    // Add x, y, and z together and return the result.
+    return x + y;
 }
