55526a68dbc4b0dd81782ff051b2975f00ab7d5d
hs_me <hsuthar326@gmail.com>
2023-11-21 23:10:06 +0530

    Change from Laptop

--- main.cpp
+++ main.cpp
@@ -1,13 +1,41 @@
 #include <bits/stdc++.h>
+
 using namespace std;
+
+long long binaryToDecimalLarge(string binaryString)
+{
+    bitset<1000000> binary(binaryString);
+    long long decimal = binary.to_ullong();
+    return decimal;
+}
+
+string leftRotatedString(string s, int d)
+{
+    string ans = s.substr(d) + s.substr(0, d);
+    return ans;
+}
+
+string rightRotatedString(string s, int d)
+{
+    string ans = s.substr(s.length() - d) + s.substr(0, s.length() - d);
+    return ans;
+}
+
 int main()
 {
 
-    for (int i = 0; i < 10; i++)
-    {
-        cout << i << endl;
-    }
+    // Read File Input
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
 
-    cout << "Hello World!" << endl;
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int n, k;
+        cin >> n >> k;
+        string s;
+        cin >> s;   
+    }
     return 0;
 }
